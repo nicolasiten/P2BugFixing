fac136d31e9b48935d263a29825d200f20b95d1f
Nicolas Iten <nici.iten@gmail.com>
2019-08-31 19:56:12 +0200

    Removed OrderLineId --> never used

--- P2FixAnAppDotNetCode/Models/Cart.cs
+++ P2FixAnAppDotNetCode/Models/Cart.cs
@@ -113,7 +113,6 @@ namespace P2FixAnAppDotNetCode.Models
 
     public class CartLine
     {
-        public int OrderLineId { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
     }
